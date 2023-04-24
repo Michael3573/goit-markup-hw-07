bc0480ab71b99e1311e1a6e24bd52e27748981a0
Michael3573 <126564353+Michael3573@users.noreply.github.com>
2023-04-24 13:20:55 +0300

    Update1

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    Menu: document.querySelector("[data-menu]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function toggleMenu() {
+    refs.Menu.classList.toggle("is-hidden");
   }
 })();
